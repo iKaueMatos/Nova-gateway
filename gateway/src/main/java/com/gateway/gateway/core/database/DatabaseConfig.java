b2e63c9a2258bcc9b76e95823539c04500af921e
kaue matos <kaue178hotimail@gmail.com>
2024-01-28 22:12:59 -0300

    feature: adicionando arquivo de configuração do banco de dados

---
+++ gateway/src/main/java/com/gateway/gateway/core/database/DatabaseConfig.java
@@ -0,0 +1,29 @@
+/**
+ * ----------------------------------------------------------------------------
+ * Autor: Kaue de Matos
+ * Empresa: Nova Software
+ * Propriedade da Empresa: Todos os direitos reservados
+ * ----------------------------------------------------------------------------
+ */
+package com.gateway.gateway.core.database;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DatabaseConfig {
+    
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3308/novasoftware");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Nova");
+
+        return dataSource;
+    }
+}
